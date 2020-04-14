ce1ef90a2c1111074470435b9284774bcdc1fa1a
Anirban Chetia <bloodraven166@gmail.com>
2020-04-14 13:57:29 +0530

    Update Getting-a-function-name-as-a-string.r

--- Getting-a-function-name-as-a-string.r
+++ Getting-a-function-name-as-a-string.r
@@ -1,4 +1,4 @@
-// link: https://stackoverflow.com/questions/1567718/getting-a-function-name-as-a-string/59153119#59153119
+# link: https://stackoverflow.com/questions/1567718/getting-a-function-name-as-a-string/59153119#59153119
 
 workingFunction <- Function(f)
 { functionName <- as.character(substitute(f)) }
