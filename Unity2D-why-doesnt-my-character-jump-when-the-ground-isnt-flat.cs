6b9b7852d4c6df05be166d03fc79c2b9a78ad711
Anirban Chetia <bloodraven166@gmail.com>
2020-04-14 13:58:39 +0530

    Create Unity2D-why-doesnt-my-character-jump-when-the-ground-isnt-flat.cs

---
+++ Unity2D-why-doesnt-my-character-jump-when-the-ground-isnt-flat.cs
@@ -0,0 +1,26 @@
+// link: https://stackoverflow.com/questions/59437329/unity-2d-why-doesnt-my-character-jump-when-the-ground-isnt-flat/59437877#59437877
+
+public bool isGrounded;  
+
+//During collisions you would still want your object to jump, such as jumping while touching the corner of a wall, so:
+void OnCollisionStay()
+{
+        isGrounded = true;
+}  
+
+//Your not able to jump right after collision: (Also add layers so your object can't pass through walls or entities in the game)
+void OnCollisionExit()
+{
+        isGrounded = false;
+}
+
+//Jump only if your object is on ground:
+void Jump()
+{
+       if(isGrounded)
+       {
+          rb.AddForce (Vector2.up * jumpForce);
+          //set it to false again so you can't multi jump:
+          isGrounded = false;
+       }
+}
