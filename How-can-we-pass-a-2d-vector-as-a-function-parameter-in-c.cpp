9a20ae34977c4af255cd056b2d2894cc104c4461
Anirban Chetia <bloodraven166@gmail.com>
2020-04-14 14:05:42 +0530

    Create How-can-we-pass-a-2d-vector-as-a-function-parameter-in-c.cpp

---
+++ How-can-we-pass-a-2d-vector-as-a-function-parameter-in-c.cpp
@@ -0,0 +1,9 @@
+// link: https://stackoverflow.com/questions/60998694/how-can-we-pass-a-2d-array-vector-as-a-function-parameter-in-c/61005825#61005825 
+
+function(int array[dim1][dim2])
+
+function(int array[][dim2], int dim1)
+
+function(std::vector<std::vector<int>>const &vec)
+
+function(std::array<std::vector<int>>const &vecarray)
