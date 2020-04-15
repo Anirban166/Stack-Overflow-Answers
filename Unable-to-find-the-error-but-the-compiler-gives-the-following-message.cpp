f12b92a861005dfdf1a26c5f626858b2f5546a24
Anirban Chetia <bloodraven166@gmail.com>
2020-04-15 14:27:41 +0530

    Create Unable-to-find-the-error-but-the-compiler-gives-the-following-message.cpp

---
+++ Unable-to-find-the-error-but-the-compiler-gives-the-following-message.cpp
@@ -0,0 +1,6 @@
+// link: https://stackoverflow.com/questions/61168688/unable-the-find-the-error-but-the-compiler-gives-the-following-message/61168966#61168966
+
+std::max(lookup[i - 1][j], lookup[i][j - 1]);
+
+#define maximum 20
+int lookup[maximum][maximum];
