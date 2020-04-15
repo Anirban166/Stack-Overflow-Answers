56aa9ed8038f74dae2b15c8c76d8d50eab0c1c52
Anirban Chetia <bloodraven166@gmail.com>
2020-04-15 14:36:49 +0530

    Create C++-function-to-split-strings.cpp

---
+++ C++-function-to-split-strings.cpp
@@ -0,0 +1,7 @@
+// link: https://stackoverflow.com/questions/61159439/c-function-to-split-strings/61159690#61159690
+
+while (i != s.size() && s[i]==',')
+   ++i;
+
+while (j != s.size() && s[j]!=',')
+   j++;
