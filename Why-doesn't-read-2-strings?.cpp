2cbae8b8362dc1c71378471eefa79472fe2db711
Anirban Chetia <bloodraven166@gmail.com>
2020-04-16 13:55:27 +0530

    Create Why-doesn't-read-2-strings?.cpp

---
+++ Why-doesn't-read-2-strings?.cpp
@@ -0,0 +1,9 @@
+// link: https://stackoverflow.com/questions/61157912/why-doesnt-read-2-strings/61158043#61158043
+
+std::getline(std::cin, str, 's');
+
+    cout<<"inserisci nb:";
+    getline(cin.ignore(), stringa1);
+
+    cout<<"inserisci m:";
+    getline(cin.ignore(), stringa2);
